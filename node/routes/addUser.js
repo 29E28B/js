e80f382c7a8612da81ae1716a826ff8cd5f97233
duriansu <89822197+duriansu@users.noreply.github.com>
2022-06-08 22:37:21 +0800

    Add files via upload

--- node/routes/addUser.js
+++ node/routes/addUser.js
@@ -1,8 +1,24 @@
 const express=require('express');
 const goodsystem = require('../sql/goodsystem');
+var multer = require('multer')
+const fs = require("fs");
 const addUser=express.Router();
-addUser.post('/adduser',(req,res)=>{
-    goodsystem.query('insert into users set ?',[req.body.data],(err,results)=>{
+addUser.post('/adduser', multer({
+    //设置文件存储路径
+    dest: 'shoppic'
+}).array('file', 1), function (req, res, next){
+    let files = req.files;
+    let file = files[0];
+    let fileInfo = {};
+    let path = 'shoppic/imgs/' + Date.now().toString() + '_' + file.originalname;
+    fs.renameSync('./shoppic/' + file.filename, path);
+    //获取文件基本信息
+    fileInfo.type = file.mimetype;
+    fileInfo.name = file.originalname;
+    fileInfo.size = file.size;
+    fileInfo.path = path;
+    Object.assign(req.body,{path:path})
+    goodsystem.query('insert into users set ?',[req.body],(err,results)=>{
         if(results){
           return  res.status(200).send({msg:'添加成功'})
         }
--- node/routes/updateUser.js
+++ node/routes/updateUser.js
@@ -1,9 +1,25 @@
 const express=require('express');
 const goodsystem = require('../sql/goodsystem');
+var multer = require('multer')
+const fs = require("fs");
+
 const updateUser=express.Router();
-updateUser.post('/updateUser',(req,res)=>{
-    const goodClear={account:req.body.data.account,username:req.body.data.username,gender:req.body.data.gender};
-    goodsystem.query('update users set ? where id=?',[goodClear,req.body.data.userid],(err,results)=>{
+updateUser.post('/updateUser', multer({
+    //设置文件存储路径
+    dest: 'shoppic'
+}).array('file', 1), function (req, res, next){
+    let files = req.files;
+    let file = files[0];
+    let fileInfo = {};
+    let path = 'shoppic/imgs/' + Date.now().toString() + '_' + file.originalname;
+    fs.renameSync('./shoppic/' + file.filename, path);
+    //获取文件基本信息
+    fileInfo.type = file.mimetype;
+    fileInfo.name = file.originalname;
+    fileInfo.size = file.size;
+    fileInfo.path = path;
+    const goodClear={account:req.body.account,username:req.body.username,gender:req.body.gender,path:path};
+    goodsystem.query('update users set ? where id=?',[goodClear,req.body.userid],(err,results)=>{
         if(results){
             return  res.status(200).send({msg:'修改成功'})
         }
